fcfb26897b4135db10010dd826470cfb60c4f225
tr <1289380467@qq.com>
2022-04-12 18:09:22 +0800

    我的第一个Java程序

---
+++ java-found/src/main/java/net/tr/java/found/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.tr.java.found.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author tr 1289380467@qq.com
+ * @version 2022/4/12 18:06
+ * @since JDK8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world with win11 & JDK8");
+    }
+}
